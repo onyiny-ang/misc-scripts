df968fe0c8df8dbb536ed7d1482cc7c2aabeb3c8
onyinyang <lt15bo@brocku.ca>
2019-02-20 12:30:22 -0500

    version 2

--- collector.sh
+++ collector.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-SOURCE_FILE=~/Documents/Brock
+SOURCE_FILE=$HOME/Documents/Brock
+SOURCE_FILE1=$SOURCE_FILE/1st-Year/
+SOURCE_FILE2=$SOURCE_FILE/3rd-Year/COSC3P99
+SOURCE_FILE3=$SOURCE_FILE/2nd-Year/COSC2P95/Lab*
 DEST_FILE=${SOURCE_FILE}/4f00-files
 
 mkdir -p ${DEST_FILE}
 
-find ${SOURCE_FILE} -type f \( -name "*.java" -or -name "*.c" -or -name "*.cpp" -or -name "*.h" -or -name "*.hpp" \) -exec cp {} ${DEST_FILE} \;
+for f in ${SOURCE_FILE1} ${SOURCE_FILE2} ${SOURCE_FILE3}; do
+    find $f -type f \( -name "*.java" -or -name "*.c" -or -name "*.cpp" -or -name "*.h" -or -name "*.hpp" \) -exec cp {} ${DEST_FILE} \;
+
+done
 
 NUM_FILES="$(ls -lq "${DEST_FILE}" | wc -l)"
 echo Found "${NUM_FILES}" files
